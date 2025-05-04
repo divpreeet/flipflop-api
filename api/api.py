c63962a3b6c939e435f0ebb2c7f4556481fcd6bf
divpreeet <ytbraced@gmail.com>
2025-05-04 12:51:49 +0530

    updated acc to docs

--- api/api.py
+++ api/api.py
@@ -57,5 +57,5 @@ def ping():
     return "pong", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000, debug=True)
-
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
