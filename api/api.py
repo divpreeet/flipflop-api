17b9cac11608b6fa607bbeb7f1daf4b6c047148f
divpreeet <ytbraced@gmail.com>
2025-05-04 12:01:20 +0530

    addded ping func cause i hate render

--- api/api.py
+++ api/api.py
@@ -51,5 +51,9 @@ def api_convert(fmt):
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
+@app.route('/ping', methods=['GET'])
+def ping():
+    return "pong", 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
