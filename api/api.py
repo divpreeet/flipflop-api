212872da9b7c7b459f82ee4ecbbf1099eac88ccd
divpreeet <ytbraced@gmail.com>
2025-05-08 15:35:12 +0530

    added render.yaml'

--- api/api.py
+++ api/api.py
@@ -57,5 +57,5 @@ def ping():
     return "pong", 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
